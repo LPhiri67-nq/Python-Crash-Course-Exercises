bb53b0478c2352737e65c552f95864c7108a0261
LPhiri67-nq <phirilinette7@gmail.com>
2026-03-04 21:25:27 +0000

    Updated Changes

--- CourseExercises/Chp_5_if_statements_1.3.py
+++ CourseExercises/Chp_5_if_statements_1.3.py
@@ -6,12 +6,12 @@
 # Make a list of five or more usernames called current_users.
 current_users = ["Carlos Lyons", "Kenzie Palacios", "Devon Atkins",
                  "Salem Austin", "Legacy Davila", "Noe Estes"]
-current_users_lowercase = current_users[:]
+current_users_lowercase = [current_user.lower() for current_user in current_users]
 
 # Make another list of five usernames called new_users.
 # Make sure one or two of the new usernames are also in the current_users list.
 
-new_users = ["Egypt Pena", "Adelynn Hendricks", "Abby Marshall", "Legacy Davila"]
+new_users = ["Egypt Pena", "Adelynn Hendricks", "Abby Marshall", "legacy davila"]
 
 # Loop through the new_users list...
 # ...to see if each new username has already been used.
@@ -29,8 +29,44 @@ new_users = ["Egypt Pena", "Adelynn Hendricks", "Abby Marshall", "Legacy Davila"
 
 for new_user in new_users:
     if new_user in current_users:
-        print("This username already exists. Enter a different username")
+        print(f"\n{new_user} already exists. Enter a different username")
     elif new_user.lower() in current_users_lowercase:
-        print("This username already exists. Enter a different username")
+        print(f"\n{new_user.lower()} already exists. Enter a different username.")
     else:
-        print("Username is available.\nWelcome, New User!")
+        print(f"{new_user} is available.\nWelcome, New User!")
+
+# Exercise 5-11: Ordinal Numbers
+
+# Ordinal numbers indicate their position in a list, such as 1st or 2nd.
+# Most ordinal numbers end in th, except 1, 2, and 3.
+# Store the numbers 1 through 9 in a list.
+
+numbers_from_1_to_9 = list(range(1,10))
+# Loop through the list
+
+# Use an if-elif-else chain inside the loop...
+# ...to print the proper ordinal ending for each number.
+
+# Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th",...
+# ...and each result should be on a separate line.
+
+ordinal_numbers = []
+
+for number in numbers_from_1_to_9:
+    if number == 1:
+        print(f"\nThe ordinal ending for the number {number} is 'st")
+        ordinal_numbers.append("1st")
+    elif number == 2:
+        print(f"The ordinal ending for the number {number} is 'nd")
+        ordinal_numbers.append("2nd")
+    elif number == 3:
+        print(f"The ordinal ending for the number {number} is 'rd.")
+        ordinal_numbers.append("3rd")
+    else:
+        print(f"The ordinal ending for the number {number} is 'th")
+        ordinal_numbers.append(f"{number}th")
+
+print(f"\n{ordinal_numbers}")
+
+for ordinal in ordinal_numbers:
+    print(ordinal)
